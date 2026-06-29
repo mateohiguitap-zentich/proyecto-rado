e92ad58442cb359612a16f86595081f8979ae7c4
mateohiguitap <mateohiguitap@gmail.com>
2026-06-29 16:39:33 -0500

    Estructura unificada frontend y backend con api

---
+++ proyecto-rado-backendAPI/src/main/java/com/zentich/proyecto_rado_backendAPI/ProyectoRadoBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.zentich.proyecto_rado_backendAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRadoBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoRadoBackendApiApplication.class, args);
+	}
+
+}
---
+++ proyecto-rado-backendAPI/src/test/java/com/zentich/proyecto_rado_backendAPI/ProyectoRadoBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zentich.proyecto_rado_backendAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoRadoBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
